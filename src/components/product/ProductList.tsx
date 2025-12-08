d2a84bb4d455d82dae95ab381f2a4b33f5772ab5
amirhossein-k <amirhossein.k787878@gmail.com>
2025-12-08 21:23:55 +0330

    fvvfev

--- src/components/product/ProductList.tsx
+++ src/components/product/ProductList.tsx
@@ -1,20 +1,20 @@
 // components/products/ProductList.tsx
-import { FormattedPostType, Product } from "@/types";
-import ProductCard from "./ProductCard";
+// import { FormattedPostType, Product } from "@/types";
+// import ProductCard from "./ProductCard";
 
-export default function ProductList({
-  products,
-}: {
-  products: FormattedPostType[];
-}) {
-  if (!products?.length) {
-    return <p>محصولی یافت نشد</p>;
-  }
-  return (
-    <div>
-      {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
-    </div>
-  );
-}
+// export default function ProductList({
+//   products,
+// }: {
+//   products: FormattedPostType[];
+// }) {
+//   if (!products?.length) {
+//     return <p>محصولی یافت نشد</p>;
+//   }
+//   return (
+//     <div>
+//       {products.map((product) => (
+//         <ProductCard key={product.id} product={product} />
+//       ))}
+//     </div>
+//   );
+// }
